a7346da8f94e0f60dcaf2d8509d2b3526ec4c7bd
Aidan Dunn <aidancheyd+gitkraken@gmail.com>
2020-12-29 17:10:20 +1300

    feat: creates service for handling filter based systems

---
+++ src/app/filter.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+
+  private readonly _filter = new BehaviorSubject<string>('');
+  public readonly filterTerm$ = this._filter.asObservable().pipe( shareReplay(1) );
+  public get filterTerm() { return this._filter.getValue(); }
+  public set filterTerm(term: string) { this._filter.next(term); }
+
+  private readonly _filterable = new BehaviorSubject<boolean>(false);
+  public filterable$ = this._filterable.asObservable().pipe( shareReplay(1) );
+  public get filterable() { return this._filterable.getValue(); }
+  public set filterable(bool: boolean) { this._filterable.next(bool); }
+
+  constructor() { }
+}
