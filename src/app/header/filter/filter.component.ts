8407ff330676721d0e2611a7a167047b0df71b6e
Aidan Dunn <aidancheyd+gitkraken@gmail.com>
2020-12-29 22:56:58 +1300

    feat: adds filter field clear and open/close logic

--- src/app/header/filter/filter.component.ts
+++ src/app/header/filter/filter.component.ts
@@ -1,30 +1,32 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Subject } from 'rxjs';
-import { takeUntil, tap } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnInit, OnDestroy {
   /** Outputs the filter field's current value */
   @Output() filter = new EventEmitter<string>();
+  /** Reference to the filter input control */
+  @ViewChild('filterInput') filterInputRef?: ElementRef<HTMLInputElement>;
   /** Boolean to control the filter field's visible state */
-  public filterOpen = true;
+  public filterOpen = false;
   /** Control to hold the value of the filter field */
   public readonly filterControl = new FormControl();
   /** Emits when the component is destroyed */
   private readonly destruction$ = new Subject();
-
   constructor(
     private readonly iconRegistry: MatIconRegistry,
     private readonly sanitizer: DomSanitizer,
   ) {
     this.iconRegistry.addSvgIcon( 'search', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/magnifying-glass.svg') );
+    this.iconRegistry.addSvgIcon( 'close', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/plus.svg') );
   }
 
   ngOnInit(): void {
@@ -37,6 +39,18 @@ export class FilterComponent implements OnInit {
 
   ngOnDestroy() {
     this.destruction$.next();
+  }
+
+  public openFilter(event: Event) {
+    this.filterOpen = true;
+    setTimeout( () => {
+      if( !!this.filterInputRef ) this.filterInputRef.nativeElement.focus();
+    }, 100 );
+  }
+
+  public onBlur() {
+    if ( !!this.filterControl.value ) return;
+    this.filterOpen = false;
   }
 
 }
