85dcec17efaeef4cdcd16c46cf7b20323bba92fa
Aidan Dunn <aidancheyd@gmail.com>
2021-01-06 23:12:02 +1300

    feat(list-item): adds edit button to items

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Item } from '../item.model';
+import { NewItemComponent } from '../new-item/new-item.component';
 
 @Component({
   selector: 'app-list-item',
@@ -11,9 +15,24 @@ export class ListItemComponent implements OnInit {
 
   @Input() item = {} as Item;
 
-  constructor( ) { }
+  constructor(
+    private readonly iconRegistry: MatIconRegistry,
+    private readonly sanitizer: DomSanitizer,
+    private readonly matDialog: MatDialog,
+  ) {
+    this.iconRegistry.addSvgIcon( 'edit', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/pencil.svg') );
+  }
 
   ngOnInit(): void {
+  }
+
+  /** Open item editing dialog */
+  editItem( item: Item ) {
+    this.matDialog.open( NewItemComponent, { data: item } ).afterClosed().subscribe( res => {
+      if ( !!res ) {
+        
+      }
+    } );
   }
 
 }
