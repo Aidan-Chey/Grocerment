79e8d28ea623681cfbbbee53683565fb7223d651
Aidan Dunn <aidancheyd@gmail.com>
2021-01-07 13:34:29 +1300

    feat: adds firebase connection details to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyDEKN2rbrU6V7oleIC5zvFvfarYjMJhgJ8",
+    authDomain: "grocerment.firebaseapp.com",
+    projectId: "grocerment",
+    storageBucket: "grocerment.appspot.com",
+    messagingSenderId: "575679077553",
+    appId: "1:575679077553:web:81fe39aecdd4a1fa6e5690"
+  }
 };
