269db6e09379622031423f43b3dc960e01c59f8a
Aidan Dunn <aidancheyd@gmail.com>
2021-01-24 15:54:35 +1300

    refactor: removes redundant script code

--- src/assets/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-String.prototype.capitalizeFirstLetter = function() {
-	return this.charAt(0).toUpperCase() + this.slice(1);
-}		//http://stackoverflow.com/a/1026087
-function isJson(str) {
-	try { JSON.parse(str); } catch (e) { return false; }
-	return true;
-}		//http://stackoverflow.com/a/9804835
