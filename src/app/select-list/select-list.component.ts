85b79196fd41f2028dcfd1a36bbe1e696e39b1d9
Aidan Dunn <aidancheyd@gmail.com>
2021-02-01 21:36:50 +1300

    feat(select-list): adds ability to delete lists and other WIP features

--- src/app/select-list/select-list.component.ts
+++ src/app/select-list/select-list.component.ts
@@ -1,14 +1,18 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { MatDialogRef } from '@angular/material/dialog';
-import { map, shareReplay, withLatestFrom } from 'rxjs/operators';
-import { combineLatest } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, filter, map, shareReplay, switchMap, take, withLatestFrom } from 'rxjs/operators';
+import { combineLatest, EMPTY, from, of } from 'rxjs';
 import { List } from '../models/list.model';
 import { ListService } from '../services/list.service';
-import { MatSelectionList } from '@angular/material/list';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { confirmData, ConfirmDialog } from '../confirm/confirm.dialog';
+import { environment } from 'src/environments/environment';
+import * as Sentry from '@sentry/angular';
+import { RenameDialog } from './rename/rename.dialog';
 
 export const selectListConfig = {
 	minWidth: '5em',
@@ -24,26 +28,7 @@ export const selectListConfig = {
 })
 export class SelectListComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('listsList') listsList?: MatSelectionList;
-
   public readonly lists$ = this.firestore.collection<List>('lists').valueChanges({idField: 'id'}).pipe(
-    shareReplay(1),
-  );
-
-  public readonly listsOptions$ = combineLatest([
-    this.lists$,
-    this.listService.activeListSubject.asObservable(),
-  ]).pipe(
-    map( ([lists, activeList]) => Array.isArray(lists) ? lists.reduce( (acc,cur) => {
-      // Don't include currently active list in selction
-      if ( activeList?.id !== cur.id ) acc.push( { name: cur.name, id: cur.id, users: cur.users?.length } );
-      return acc;
-    }, [] as { name: string, id: string | null, users: number | undefined }[] ) : undefined ),
-    withLatestFrom(this.afAuth.user),
-    map( ([lists,user]) => {
-      if ( !!user ) lists?.unshift({ name: 'Default', users: 1, id: null });
-      return lists;
-    } ),
     shareReplay(1),
   );
 
@@ -52,17 +37,112 @@ export class SelectListComponent implements OnInit, AfterViewInit {
     private readonly sanitizer: DomSanitizer,
     private readonly firestore: AngularFirestore,
     private readonly afAuth: AngularFireAuth,
-    private readonly listService: ListService,
-    public readonly dialogRef: MatDialogRef<SelectListComponent>,
+    private readonly snackbar: MatSnackBar,
+    public readonly listService: ListService,
+    public readonly dialog: MatDialog,
   ) {
-    this.iconRegistry.addSvgIcon( 'cancel', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/plus.svg') );
+    this.iconRegistry.addSvgIcon( 'plus', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/plus.svg') );
+    this.iconRegistry.addSvgIcon( 'options', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/options.svg') );
+    this.iconRegistry.addSvgIcon( 'list', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/list.svg') );
   }
 
   ngOnInit(): void {
+
   }
 
   ngAfterViewInit() {
-
+    
   }
+
+  // Updates active list
+  setActiveList( list: List ) {
+    if ( !!list ) this.listService.activeListSubject.next(list);
+  }
+
+  /** Delete a list */
+  deleteList( list: List ) {
+    if ( !list.id ) return;
+
+    const toDelete = list.id;
+
+    const data = {
+      title: 'Are you sure?',
+      content: `You are about to delete the list ${list.name}? Everyone will loose access to the list and it's items will be deleted; this data will not be recoverable!`,
+      accept: 'Delete list',
+      decline: 'Cancel',
+    } as confirmData;
+
+    // activate dialog as check
+    this.dialog.open( ConfirmDialog, { data } ).afterClosed().pipe(
+      take(1),
+      filter(choice => !!choice ),
+      switchMap( () => this.firestore.collection<List>('lists').doc(toDelete).delete() ),
+      catchError( err => {
+        const issue = 'Failed to delete list';
+        if ( environment.production ) Sentry.captureException(err);
+        else console.error(issue + ' |', err);
+        this.snackbar.open( issue, 'Dismiss', { duration: 3000, verticalPosition: 'top' } );
+        return EMPTY;
+      } )
+    ).subscribe( () => {
+      // List deleted successfully
+      this.snackbar.open( 'List deleted', undefined, { duration: 1000, verticalPosition: 'top' } );
+    } );
+  }
+
+  /** Change the name of a list */
+  renameList( list: List ) {
+    if ( !list.id ) return;
+
+    const toRename = list.id;
+    const data = {
+      name: list.name,
+    }
+    this.dialog.open( RenameDialog, { data } ).afterClosed().pipe(
+      
+      take(1),
+      filter(revision => !!revision ),
+      switchMap( revision => this.firestore.collection<List>('lists').doc(toRename).update(revision) ),
+      catchError( err => {
+        const issue = 'Failed to rename list';
+        if ( environment.production ) Sentry.captureException(err);
+        else console.error(issue + ' |', err);
+        this.snackbar.open( issue, 'Dismiss', { duration: 3000, verticalPosition: 'top' } );
+        return EMPTY;
+      } )
+    ).subscribe( () => {
+      // List renamed successfully
+      this.snackbar.open( 'List renamed', undefined, { duration: 1000, verticalPosition: 'top' } );
+    } );
+    
+  }
+
+  /** Add or remove users from a list */
+  editListUsers( list: List ) {
+    if ( !list.id ) return;
+
+    const toEdit = list.id;
+
+    const toRename = list.id;
+    const data = {
+      name: list.name,
+    }
+    this.dialog.open( RenameDialog, { data } ).afterClosed().pipe(
+      
+      take(1),
+      filter(revision => !!revision ),
+      switchMap( revision => this.firestore.collection<List>('lists').doc(toRename).update(revision) ),
+      catchError( err => {
+        const issue = 'Failed to rename list';
+        if ( environment.production ) Sentry.captureException(err);
+        else console.error(issue + ' |', err);
+        this.snackbar.open( issue, 'Dismiss', { duration: 3000, verticalPosition: 'top' } );
+        return EMPTY;
+      } )
+    ).subscribe( () => {
+      // List renamed successfully
+      this.snackbar.open( 'List renamed', undefined, { duration: 1000, verticalPosition: 'top' } );
+    } );
+  } 
 
 }
--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { BehaviorSubject, EMPTY, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { List } from '../models/list.model';
+import * as Sentry from '@sentry/angular';
+import { catchError, shareReplay, switchMap, take, takeUntil } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +14,67 @@ import { List } from '../models/list.model';
 export class ListService {
 
   public readonly activeListSubject = new BehaviorSubject<List | undefined>(undefined);
+  
+  public readonly lists$ = this.firestore.collection<List>('lists').valueChanges({idField: 'id'}).pipe(
+    shareReplay(1),
+  );
 
-  constructor() { }
+  constructor(
+    private readonly firestore: AngularFirestore,
+    private readonly snackbar: MatSnackBar,
+    private readonly afAuth: AngularFireAuth,
+  ) {
+    // Retrieve active list from storage if set
+    try {
+      const storedlist = localStorage.getItem('activeList');
+      if ( !!storedlist && storedlist !== 'undefined' ) this.activeListSubject.next(JSON.parse(storedlist));
+    } catch (err) {
+      const issue = 'Failed to retrieving active list';
+      if ( environment.production ) Sentry.captureException(err);
+      else console.error(issue + ' |', err);
+      this.snackbar.open( issue, 'Dismiss', { duration: 3000, verticalPosition: 'top' } );
+    }
+    if ( !this.activeListSubject.getValue() ) {
+      this.lists$.pipe(
+        takeUntil(this.activeListSubject.asObservable()),
+      ).subscribe( lists => {
+        // Select a new active list
+        if ( !!Array.isArray(lists) && !!lists.length ) this.activeListSubject.next(lists[0]);
+        // Create a new list
+        else this.newList();
+      } );
+    }
+    // Saves set active list for retrieval on init
+    this.activeListSubject.asObservable().subscribe( activeList => {
+      try {
+        localStorage.setItem('activeList', JSON.stringify(activeList));
+      } catch (err) {
+        const issue = 'Failed to save active list';
+        if ( environment.production ) Sentry.captureException(err);
+        else console.error(issue + ' |', err);
+        this.snackbar.open( issue, 'Dismiss', { duration: 3000, verticalPosition: 'top' } );
+      }
+    } );
+  }
+
+  public newList() {
+    this.afAuth.user.pipe(
+      take(1),
+      switchMap( user => !!user ? this.firestore.collection<List>('lists').add({
+        users: [user.uid],
+        name: 'New List'
+      }) : of(undefined) ),
+      catchError( err => {
+        const issue = 'Failed to create list';
+        if ( environment.production ) Sentry.captureException(err);
+        else console.error(issue + ' |', err);
+        this.snackbar.open( issue, 'Dismiss', { duration: 3000, verticalPosition: 'top' } );
+        return EMPTY;
+      } )
+    ).subscribe( res => {
+      // List created successfully
+      this.snackbar.open( 'List created', undefined, { duration: 1000, verticalPosition: 'top' } );
+    } );
+  }
+
 }
