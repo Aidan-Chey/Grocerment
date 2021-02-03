34d533103943e3e0ea0d0931d18403ddaacd8837
Aidan Dunn <aidancheyd@gmail.com>
2021-02-03 20:25:28 +1300

    feat: implements user UID sharing capabilities

--- src/app/show-user-id/show-user-id.dialog.ts
+++ src/app/show-user-id/show-user-id.dialog.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
+import { environment } from 'src/environments/environment';
+import * as Sentry from '@sentry/angular';
+import { take } from 'rxjs/operators';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-show-user-id',
@@ -6,11 +14,53 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./show-user-id.dialog.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ShowUserIDDialog implements OnInit {
+export class ShowUserIDDialog implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('shareButton', { read: ElementRef }) shareButton?: ElementRef<HTMLButtonElement>;
+
+  constructor(
+    public readonly afAuth: AngularFireAuth,
+    private readonly clipboard: Clipboard,
+    private readonly snackbar: MatSnackBar,
+    private readonly dialogRef: MatDialogRef<ShowUserIDDialog>,
+  ) {
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit() {
+    this.shareButton?.nativeElement.addEventListener( 'click', event => {
+      this.afAuth.user.pipe( take(1) ).subscribe( user => {
+        if ( !user ) return;
+        if ( !!navigator.share ) {
+          navigator.share({
+            title: 'Grocerment user UID',
+            text: user.uid,
+          }).then(() => {
+            this.snackbar.open( 'Shared', undefined, { duration: 1000, verticalPosition: 'top' } );
+          })
+          .catch( err => {
+            const issue = 'Failed to copy';
+            if ( environment.production ) Sentry.captureException(err);
+            else console.error(issue + ' |', err);
+            this.snackbar.open( issue, undefined, { duration: 2000, verticalPosition: 'top', panelClass: "error" } );
+          });
+          return;
+        }
+        if ( !!this.clipboard.copy(user.uid) ) {
+          this.snackbar.open( 'Copied', undefined, { duration: 1000, verticalPosition: 'top' } );
+          this.dialogRef.close();
+        } else {
+          this.snackbar.open( 'Failed to copy', undefined, { duration: 2000, verticalPosition: 'top', panelClass: "error" } );
+        }
+      } );
+    } );
+  }
+
+  public addToClipboard( event: Event, toCopy: string ) {
+    
   }
 
 }
