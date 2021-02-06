d83133c5f0180cafc1917abc92131e5f74cca0d4
Aidan Dunn <aidancheyd@gmail.com>
2021-02-06 14:40:50 +1300

    chore(confirm): corrects syntax of confirm data type

--- src/app/confirm/confirm.dialog.ts
+++ src/app/confirm/confirm.dialog.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 /** Expected object for injected dialog data */
-export type confirmData = {
+export type ConfirmData = {
 	/** String to use as dialog title, replacing the default */
 	title?: string
 	/** String to use as dialog content */
@@ -21,7 +21,7 @@ export type confirmData = {
 export class ConfirmDialog implements OnInit {
 
 	constructor(
-		@Inject(MAT_DIALOG_DATA) public data: confirmData,
+		@Inject(MAT_DIALOG_DATA) public data: ConfirmData,
 	) { }
 
 	ngOnInit(): void {
